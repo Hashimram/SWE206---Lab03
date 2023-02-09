cdd31b26d35012fe50c37d1f7287702c3bc5ac89
Hashim Abdullah <Hashimram>
2023-02-09 14:12:26 +0300

    implemented selection sort method

--- Sorter.java
+++ Sorter.java
@@ -1,5 +1,24 @@
 public class Sorter {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        
+    }
+
+    public static void selectionsort(double[] list){
+        for (int i = 0; i<list.length - 1; i++){
+            double currentMin = list[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < list.length;j++) {
+                if (currentMin > list[i]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
     }
 }
